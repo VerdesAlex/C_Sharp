e1bab1188890d827867dc1466003da86bc6dbfc9
Marmota_in_coma <40302782+Mahou-sama666@users.noreply.github.com>
2024-03-05 13:11:31 +0200

    Add files via upload

---
+++ Seminar 3/S3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+    
+namespace S3
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        
+            Form formular=new Form();
+            formular.SuspendLayout();
+            formular.StartPosition = FormStartPosition.Manual;
+            formular.FormBorderStyle = FormBorderStyle.Sizable;
+            formular.Text = " Formular aplicatie windows";
+
+            formular.SetBounds(10,10,500,500);
+
+            Label lbl1=new Label();
+            lbl1.Text = "Eticheta detalii: ";
+            lbl1.Location = new Point(50, 50);
+            lbl1.Size = new System.Drawing.Size(150, 30);
+
+            TextBox tb1= new TextBox();
+            tb1.Location = new Point(180, 50);
+            tb1.Size = new Size(150, 30);
+
+            Button btn1 = new Button()
+            {
+                Size = new Size(70, 40),
+                Location = new Point(50, 150),
+                Text = "Afiseaza"
+            };
+            formular.Controls.Add(btn1);
+            formular.Controls.Add(lbl1);
+            formular.Controls.Add(tb1);
+            formular.ResumeLayout();
+
+            btn1.Click += (obj, s) => {
+                DialogResult raspuns = MessageBox.Show("Text afisat", "fereastra mesaj",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Stop);
+                if (raspuns == DialogResult.Cancel)
+                {
+                    MessageBox.Show("Raspuns gresit!");
+                } 
+            };
+
+            Application.Run(formular);
+        }
+    }
+}
---
+++ Seminar 3/S3FormApp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S3FormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nigger_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult rezultat;
+            rezultat = MessageBox.Show("Sup bro?",  "Nigga", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (rezultat == DialogResult.Cancel)
+                MessageBox.Show("Esti gay");
+            else
+                MessageBox.Show("Nu esti gay");
+        }
+    }
+}
